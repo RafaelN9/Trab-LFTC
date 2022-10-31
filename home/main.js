7294d5af6b9962bc6154010195c8ff8b0c213342
RafaelN9 <rafael.n.correia@unesp.br>
2022-10-31 17:03:01 -0300

    Bootstrap and jquery

--- home/main.js
+++ home/main.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 
+const electron = require('electron')
+
+// Enable live reload for all the files inside your project directory
+require('electron-reload')(__dirname);
+
 const createWindow = () => {
 const win = new BrowserWindow({
     width: 800,
